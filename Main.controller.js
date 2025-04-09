f395ae935c6f95ac8b99c45ac8dbbf1ca869b087
Gilles Muyshondt <gilles.muyshondt@yteria.com>
2025-04-09 09:30:25 +0000

    test

--- Main.controller.js
+++ Main.controller.js
@@ -6,13 +6,32 @@ sap.ui.define([
 
 	return Controller.extend("my.component.sample.cardContentControls.Main", {
 		onInit: function () {
-			var oModel = new JSONModel({
+			// var oModel = new JSONModel({
 				
-				"numbers": [
-					1, 2, 3, 4, 5,6
-				]
+			// 	"numbers": [
+			// 		1, 2, 3, 4, 5,6
+			// 	]
+			// });
+			const oView =  this.getView();
+			console.log("test")
+			
+			const apiUrl = `https://catfact.ninja/fact`;
+//f
+			fetch(apiUrl)
+			.then(response => {
+				if (!response.ok) throw new Error('Failed to fetch weather data');
+				return response.json();
+				
+			})
+			.then(data => {
+				
+				const oWeatherModel = new JSONModel(data);
+      			oView.setModel(oWeatherModel, "cat");
+				
+			})
+			.catch(error => {
+				console.error('Error fetching weather:', error);
 			});
-			this.getView().setModel(oModel);
 		}
 	});
 });
